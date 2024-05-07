33de3aa07970a44c0f3070d3f763e231d7f212cf
matteofresta <matteofresta@hotmail.com>
2024-05-07 17:01:58 +0200

    sehenswert

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('videoPlaceholder').addEventListener('click', function () {
     this.style.display = 'none';
-    var iframe = document.getElementById('videoIframe');
+    let iframe = document.getElementById('videoIframe');
     iframe.src = "https://www.youtube.com/embed/Mdcw3Sb98DA?si=7S2cISYFoz-j7bzm&autoplay=1";
     iframe.classList.add('visible');
 });
